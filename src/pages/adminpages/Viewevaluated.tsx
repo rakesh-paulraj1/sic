5918ace1292f7e8f151b2febc842f0afb3d0a59c
Rakesh.P <168630658+rakesh-paulraj1@users.noreply.github.com>
2024-12-13 21:30:04 +0530

    project done

--- src/pages/adminpages/Viewevaluated.tsx
+++ src/pages/adminpages/Viewevaluated.tsx
@@ -12,54 +12,56 @@ interface Idea {
   idea_title: string;
   status_id: number;
   theme_id: number;
-  type: string;
+  idea_type: string;
   idea_description: string;
   assigned_count: number;
   status:string
+  novelty_score:string
+  usefulness_score:string;
+  feasibility_score:string
+  scalability_score:string
+  sustainability_score:string
 }
 
 export function Viewevaluated() {
   const { idea_id } = useParams();
   
-  const [ideas, setIdeas] = useState<Idea[]>([]);
-
-  
+  const [idea, setIdeas] = useState<Idea | undefined>();
 
   const navigate = useNavigate(); 
 
   useEffect(() => {
     const role=localStorage.getItem("role");
     const evaluatorId=localStorage.getItem("user_id");
-if(role!="evaluator"){
-  navigate("/");
-}
-   console.log(evaluatorId );
-   console.log(idea_id);
-axios
-.post(
-  `${BACKEND_URL}getideascore.php`,
-  {
-    evaluator_id: evaluatorId,
-    idea_id: idea_id,
-  },
-  {
-    withCredentials: true,
-  }
-)
-.then((response) => {
-  console.log(response.data.data);
-  const data = Array.isArray(response.data.data) ? response.data.data : [];
-  setIdeas(data);
-})
-.catch((e:string) => {
- console.log("Error"+e);
-});
-}, [navigate]);
+    if(role!="evaluator"){
+      navigate("/");
+    }
+ 
+    axios
+    .post(
+      `${BACKEND_URL}getideascore.php`,
+      {
+        evaluator_id: evaluatorId,
+        idea_id: idea_id,
+      },
+      {
+        withCredentials: true,
+      }
+    )
+    .then((response) => {
+      console.log(response);
+      setIdeas(response.data.data);
+      console.log(response.data.data);
+
+      console.log(idea);
+    })
+    .catch((e:string) => {
+     console.log("Error"+e);
+    });
+  }, [navigate, idea_id]);
 
   return (
     <div className="w-full max-w-full">
-  
-   
      <Navbar />
      <EvaluatorTopbar/>
      
@@ -67,43 +69,39 @@ axios
           <button className="bg-cyan-900 mt-4 ml-3 text-white py-2 px-4 rounded-md hover:bg-cyan-800">Back</button>
         </Link>
       <div className="bg-white p-6 rounded-lg shadow-lg">
-        <h1 className="text-3xl font-semibold mb-6">Evaluated scores</h1>
+        <h1 className="text-4xl font-bold mb-6">Evaluated scores</h1>
         <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {/* Idea Details */}
           <div className="space-y-2">
-            <p className=" text-xl font-medium text-gray-600"><strong>Title:</strong></p>
-            <p className="text-gray-800">{idea.idea_title}</p>
+            <p className=" text-3xl font-medium text-black-600"><strong>Title:</strong></p>
+            <p className="text-xl text-black-800">{idea?.idea_title}</p>
+          </div>
+
+
+          <div className="space-y-2">
+            <p className=" text-3xl font-medium text-black-600"><strong>Student Name:</strong></p>
+            <p className="text-xl text-black-800">{idea?.student_name}</p>
           </div>
 
           <div className="space-y-2">
-            <p className=" text-xl font-medium text-gray-600"><strong>Description:</strong></p>
-            <p className="text-gray-800">{idea.idea_description}</p>
+            <p className=" text-3xl font-medium text-black-600"><strong>School:</strong></p>
+            <p className=" text-xl text-black-800">{idea?.school}</p>
           </div>
 
           <div className="space-y-2">
-            <p className=" text-xl font-medium text-gray-600"><strong>Student Name:</strong></p>
-            <p className="text-gray-800">{idea.student_name}</p>
+            <p className=" text-3xl font-medium text-black-600"><strong>Idea type:</strong></p>
+            <p className=" text-xl text-black-800">{idea?.idea_type}</p>
           </div>
-
+          
           <div className="space-y-2">
-            <p className=" text-xl font-medium text-gray-600"><strong>School:</strong></p>
-            <p className="text-gray-800">{idea.school}</p>
+            <p className=" text-3xl font-medium text-black-600"><strong>Description:</strong></p>
+            <p className="text-black-800">{idea?.idea_description}</p>
           </div>
-
-          <div className="space-y-2">
-            <p className=" text-xl font-medium text-gray-600"><strong>Type:</strong></p>
-            <p className=" text-md text-gray-800">{idea.type}</p>
-          </div>
-
-
-         
-
-
         </div>
   <div className="mr-20 ml-20 mt-10 mb-6 bg-white rounded-lg overflow-hidden">
 
-    <table className="w-full table-auto border border-gray-300">
-      <thead className="bg-gray-100">
+    <table className="w-full table-auto border border-black-300">
+      <thead className="bg-black-100">
         <tr>
           <th className="border px-4 py-2">Novelty Score</th>
           <th className="border px-4 py-2">Usefulness Score</th>
@@ -113,22 +111,14 @@ axios
         </tr>
       </thead>
       <tbody>
-        {ideas.length === 0 ? (
-          <tr>
-            <td colSpan={5} className="text-center py-4 text-gray-500">
-              No ideas assigned
-            </td>
+        {idea && (
+          <tr key={idea.id} className="hover:bg-gray-50 text-lg text-center">
+           <td>{idea.novelty_score}</td>
+           <td>{idea.usefulness_score}</td>
+           <td>{idea.feasibility_score}</td>
+           <td>{idea.scalability_score}</td>
+           <td>{idea.sustainability_score}</td>
           </tr>
-        ) : (
-          ideas.map((idea) => (
-            <tr key={idea.id} className="hover:bg-gray-50">
-             <td> {idea.novelty_score}</td>
-             <td> {idea.usefulness_score}</td>
-             <td> {idea.feasibility_score}</td>
-             <td> {idea.scalability_score}</td>
-             <td> {idea.sustainability_score}</td>
-            </tr>
-          ))
         )}
       </tbody>
     </table>
